b57083487fd5ebda6813622d132792b1bbf1a096
Ranjan Das <msd23012@iiitl.ac.in>
2025-02-18 23:48:37 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networkSecurity.components.data_ingestion import DataIngestion
 from networkSecurity.components.data_validation import DataValidation
+from networkSecurity.components.data_transformation import DataTransformation
 from networkSecurity.exception.exception import NetworkSecurityException
 from networkSecurity.logging.logger import logging
-from networkSecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networkSecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networkSecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -26,7 +27,13 @@ if __name__=='__main__':
         logging.info("Data validation completed.")
         print(data_validation_artifact)
 
-        
-       
+        ## Data Transformation
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        logging.info("Data Transformation started.")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation completed.")
+        print(data_transformation_artifact)
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networkSecurity/entity/artifact_entity.py
+++ networkSecurity/entity/artifact_entity.py
@@ -5,7 +5,6 @@ class DataIngestionArtifact:
     trained_file_path: str
     test_file_path: str
 
-
 @dataclass
 class DataValidationArtifact:
     validation_status: bool
@@ -13,4 +12,10 @@ class DataValidationArtifact:
     valid_test_file_path: str
     invalid_train_file_path: str
     invalid_test_file_path: str
-    drift_report_file_path: str    
+    drift_report_file_path: str  
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str  
